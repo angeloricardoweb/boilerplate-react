2be100d31c044d165b390e2e3462ce6e60fda34a
Reza Rachmanuddin <39640211+retry19@users.noreply.github.com>
2022-10-21 18:24:53 +0000

    feat: page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
+import PageNotFound from './pages/404'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { DrawerMenu } from './components/DrawerMenu'
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contato" element={<Contato />} />
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
           <Footer />
         </DrawerMenu>
