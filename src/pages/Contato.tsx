1e2a53d162588e90a179779b47956a79e3c3b292
Angelo Ricardo <angeloengcomp@gmail.com>
2023-01-11 17:12:13 -0300

    feat: add float input example

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -4,6 +4,13 @@ export default function Contato() {
   return (
     <div className="main_container">
       <div className="text-center ">Contato</div>
+      <div className="label-float mt-3">
+        <input
+          type="email"
+          placeholder=" "
+        />
+        <label>E-mail</label>
+      </div>
     </div>
   );
 }
