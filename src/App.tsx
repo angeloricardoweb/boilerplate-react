1207e84d3835e38ded663159130d4823bc15422f
JoaoCardoso00 <jvcq13@gmail.com>
2023-02-09 11:00:01 -0300

    added initial styles for new home screen

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <GlobalContextProvider>
       <BrowserRouter>
         <DrawerMenu>
-          <Header />
+          {/* <Header /> */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contato" element={<Contato />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,60 @@
-import React from 'react'
-import SectionQualquer from '../components/Sections/SectionQualquer'
-
 export default function Home() {
   return (
-    <div className='main_container'>
-      <div className="text-center">Home</div>
-      <section>
-        <SectionQualquer />
-      </section>
-    </div>
-  )
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#202024] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-10 ">
+          <img src="/icone-bredi.svg" className="h-44" />
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            <span className="text-[#F7D622]">Bredi</span> Template
+          </h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-4 xl:gap-8">
+            <a
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://tailwindcss.com"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold text-white">Tailwind →</h3>
+              <div className="text-lg">
+                Documentação do nosso pré-processador CSS.
+              </div>
+            </a>
+            <a
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://react-hook-form.com"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold text-white">
+                React-hook-form →
+              </h3>
+              <div className="text-lg">
+                Documentação da nossa biblioteca de validação de formulários.
+              </div>
+            </a>
+            <a
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://daisyui.com"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold text-white">Daisy UI →</h3>
+              <div className="text-lg">
+                Documentação da nossa principal biblioteca de componentes.
+              </div>
+            </a>
+            <a
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://swr.vercel.app"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold text-white">SWR →</h3>
+              <div className="text-lg">
+                Documentação da nossa principal biblioteca para consumo de
+                dados.
+              </div>
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
