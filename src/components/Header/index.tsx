bf785a42d06a5527237a61830ac25761c7a80d8e
JoaoCardoso00 <jvcq13@gmail.com>
2023-02-10 11:58:29 -0300

    added daisyui navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
 export function Header() {
   return (
-    <header >
-      <Link to="/">Home</Link>
-      <Link to="/contato">Contato</Link>
-      <label htmlFor="my-drawer-4" className="md:hidden border">
-        Open drawer
-      </label>
-    </header>
-  )
+    <div className="navbar fixed">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-circle btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+          >
+            <li>
+              <a>Homepage</a>
+            </li>
+            <li>
+              <a>Form Example</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
+
