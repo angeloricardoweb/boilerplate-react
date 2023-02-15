738a83b9662480c69d652e82b8ef39d8aa295d71
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-15 11:29:53 -0300

    feat: add custom hook useEffectOnScroll

--- src/context/GlobalContextProvider.tsx
+++ src/context/GlobalContextProvider.tsx
@@ -1,14 +1,16 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useState, createContext, useContext, useRef } from 'react'
 import { GlobalContextData } from './type'
 
 export const GlobalContext = createContext<GlobalContextData>({} as GlobalContextData)
 
 export function GlobalContextProvider({ children }: { children: React.ReactNode }) {
   const [swiperNoticiaInstance, setSwiperNoticiaInstance] = useState()
+  const drawerMenuRef = useRef()
 
   const data = {
     swiperNoticiaInstance,
     setSwiperNoticiaInstance,
+    drawerMenuRef
   }
 
   return (
--- src/hooks/useEffectOnScroll.ts
+++ src/hooks/useEffectOnScroll.ts
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useGlobalProvider } from '../context/GlobalContextProvider';
+import { useGlobal } from '../context/GlobalContextProvider';
 
 export function useEffectOnScroll() {
   const [scroll, setScroll] = useState(0);
-  const { drawerMenuRef } = useGlobalProvider() as any;
+  const { drawerMenuRef } = useGlobal();
 
   useEffect(() => {
-    const onScroll = () => setScroll(drawerMenuRef.current.scrollTop);
+    if (drawerMenuRef?.current) {
+      const onScroll = () => setScroll(drawerMenuRef.current.scrollTop);
 
-    drawerMenuRef?.current?.removeEventListener('scroll', onScroll);
-    drawerMenuRef.current.addEventListener('scroll', onScroll);
+      drawerMenuRef?.current?.removeEventListener('scroll', onScroll);
+      drawerMenuRef.current.addEventListener('scroll', onScroll);
+    }
   }, []);
 
   return scroll;
