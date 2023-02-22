6f26b155370ee372e66bb9155dcc3554703e8fab
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-22 14:10:13 -0200

    fix: update rules and styles of button outline

--- src/components/Buttons/ButtonOutline.tsx
+++ src/components/Buttons/ButtonOutline.tsx
@@ -1,7 +1,15 @@
-import { ButtonPrimaryProps } from './type'
+import React from 'react';
 
-export default function ButtonOutiline({ children, onClick, type = 'button' }: ButtonPrimaryProps) {
+export default function ButtonOutline(
+  props: React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+) {
   return (
-    <button className="btn btn-outline" onClick={onClick} type={type}>{children}</button>
-  )
+    <button
+      {...props}
+      className="btn btn-outline flex items-center gap-2 first:hover:text-white w-full sm:w-fit"
+    />
+  );
 }
