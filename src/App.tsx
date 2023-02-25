9a1e48837b204e3424242c53be4e1ecb3b1f5d33
JoaoCardoso00 <jvcq13@gmail.com>
2023-02-24 22:29:46 -0300

    general changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,35 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { DrawerMenu } from './components/DrawerMenu';
 import { GlobalContextProvider } from './context/GlobalContextProvider';
-import { BoxCookies } from './components/Partials/BoxCookies';
+import { BoxCookies } from '@components/Partials/BoxCookies';
 
 function App() {
   return (
     <GlobalContextProvider>
       <BrowserRouter>
-        <DrawerMenu>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contato" element={<FormExample />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-          <Footer />
-          <Toaster
-            toastOptions={{
-              className: 'bg-zinc-500 text-white',
-              style: {
-                zIndex: 999999,
-              },
-            }}
-          />
-          <BoxCookies />
-        </DrawerMenu>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contato" element={<FormExample />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+        <ToasterComponent />
+        <BoxCookies />
       </BrowserRouter>
     </GlobalContextProvider>
+  );
+}
+
+function ToasterComponent() {
+  return (
+    <Toaster
+      toastOptions={{
+        className: 'bg-zinc-500 text-white',
+        style: {
+          zIndex: 999999,
+        },
+      }}
+    />
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,38 +1,4 @@
 export function Header() {
-  return (
-    <div className="navbar fixed">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-circle btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-10 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-          >
-            <li>
-              <a>Homepage</a>
-            </li>
-            <li>
-              <a>Form Example</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="fixed"></div>;
 }
 
